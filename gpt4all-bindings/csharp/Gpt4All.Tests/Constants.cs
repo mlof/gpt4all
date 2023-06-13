960a823e80d26387587dd4dc6cf2fffd570bf0f3
Marcel <marcelrlof@gmail.com>
2023-06-13 13:36:45 +0200

    Added configurability to tests

--- gpt4all-bindings/csharp/Gpt4All.Tests/Constants.cs
+++ gpt4all-bindings/csharp/Gpt4All.Tests/Constants.cs
@@ -1,10 +1,23 @@
-﻿namespace Gpt4All.Tests
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Gpt4All.Tests
 {
     public static class Constants
     {
-        public const string MODELS_BASE_DIR = "../../../models";
-        public const string LLAMA_MODEL_PATH = $"{MODELS_BASE_DIR}/ggml-gpt4all-l13b-snoozy.bin";
-        public const string GPTJ_MODEL_PATH = $"{MODELS_BASE_DIR}/ggml-gpt4all-j-v1.3-groovy.bin";
-        public const string MPT_MODEL_PATH = $"{MODELS_BASE_DIR}/ggml-mpt-7b-chat.bin";
+        public static readonly string MODELS_BASE_DIR;
+        public static readonly string LLAMA_MODEL_PATH = Path.Join(MODELS_BASE_DIR, "ggml-gpt4all-l13b-snoozy.bin");
+        public static readonly string GPTJ_MODEL_PATH = Path.Join(MODELS_BASE_DIR, "ggml-gpt4all-j-v1.3-groovy.bin");
+        public static readonly string MPT_MODEL_PATH = Path.Join(MODELS_BASE_DIR, "ggml-mpt-7b-chat.bin");
+
+        static Constants()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile("appsettings.local.json", true, true)
+                .Build();
+
+            MODELS_BASE_DIR = configuration["MODELS_BASE_DIR"] ?? throw new InvalidOperationException();
+        }
     }
 }
