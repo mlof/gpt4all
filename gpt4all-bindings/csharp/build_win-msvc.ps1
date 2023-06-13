70c7f8173da6ab58d67e4016c64a5536ca85eb6e
Marcel <marcelrlof@gmail.com>
2023-06-13 13:37:05 +0200

    updated build_win-msvc.ps1 to use the same paths as the mingw variant

--- gpt4all-bindings/csharp/build_win-msvc.ps1
+++ gpt4all-bindings/csharp/build_win-msvc.ps1
@@ -2,4 +2,4 @@ Remove-Item -Force -Recurse .\runtimes\win-x64\msvc -ErrorAction SilentlyContinu
 mkdir .\runtimes\win-x64\msvc\build | Out-Null
 cmake -G "Visual Studio 17 2022" -A X64 -S ..\..\gpt4all-backend -B .\runtimes\win-x64\msvc\build
 cmake --build .\runtimes\win-x64\msvc\build --parallel --config Release
-cp .\runtimes\win-x64\msvc\build\bin\Release\*.dll .\runtimes\win-x64
+cp .\runtimes\win-x64\msvc\build\bin\Release\*.dll .\runtimes\win-x64\native\
